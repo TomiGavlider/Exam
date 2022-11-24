fe4dbe8c212a7d2f0453197713bfa21968764033
TomiGavlider <107557530+TomiGavlider@users.noreply.github.com>
2022-11-24 11:32:33 +0100

    Create Delete comments.py
    Delete comments task

---
+++ Delete comments.py
@@ -0,0 +1,39 @@
+import os
+
+
+def deleteComments(file):
+    try:
+        my_file = open(file, 'r')
+        data = my_file.read()
+        clean = ""
+        comments_count = 0
+        for i in data.split('\n'):
+            if i[0] == "#":
+                clean += i
+                clean += '\n'
+                comments_count += 1
+            else:
+                pass
+        name = os.path.basename(path)
+        with open("clean-" + name, "w") as f:
+            f.write(clean)
+            f.close()
+        my_file.close()
+        return comments_count
+
+
+
+
+
+    except:
+        print("An error occurred with accessing the files")
+        return path
+
+
+print(deleteComments("comments.txt"))
+
+
+
+
+
+
